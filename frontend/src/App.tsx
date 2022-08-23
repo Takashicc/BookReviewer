9fb2a70a9e66abcf9b708c344c0f9d5552bbbd51
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-08-23 15:57:28 +0900

    feat: Add /books/:isbn route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import BookDetail from "./screens/BookDetail";
 import Home from "./screens/Home";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/books/:isbn" element={<BookDetail />}></Route>
         </Routes>
         <Footer />
       </Router>

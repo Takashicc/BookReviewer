6fe594f0db520b1f92c0ba4063a87382700af97a
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-08-24 06:00:54 +0900

    chore: Tell express to use static build files generated by frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,18 +3,25 @@ import cors from "cors";
 import log4js from "./config/log4js";
 import router from "./routers";
 import { errorHandler } from "./Error/ErrorHandler";
+import path from "path";
 
 const logger = log4js.getLogger();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../../frontend")));
 
 app.use("/api/v1", router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   errorHandler.handleError(err, res);
   next();
+});
+
+app.get("/*", (req, res) => {
+  console.log("request come");
+  res.sendFile(path.join(__dirname, "../../frontend/index.html"));
 });
 
 const PORT = process.env.PORT || 3001;

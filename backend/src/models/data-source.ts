1a30d611e5c93f6df92a90be325a132a30644c48
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-08-24 06:12:33 +0900

    chore: Add production config for database

--- backend/src/models/data-source.ts
+++ backend/src/models/data-source.ts
@@ -1,21 +1,36 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import CustomNamingStrategy from "./CustomNamingStrategy";
 import { Book } from "./entity/Book";
 import { Review } from "./entity/Review";
 import { User } from "./entity/User";
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "noveler",
-  password: "noveler",
-  database: "noveler",
-  entities: [User, Book, Review],
-  synchronize: true,
-  logging: false,
-  namingStrategy: new CustomNamingStrategy(),
-});
+const mode = process.env.ENVIRONMENT || "development";
+let dataSourceOptions: DataSourceOptions;
+if (mode === "development") {
+  dataSourceOptions = {
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "noveler",
+    password: "noveler",
+    database: "noveler",
+    entities: [User, Book, Review],
+    synchronize: true,
+    logging: false,
+    namingStrategy: new CustomNamingStrategy(),
+  };
+} else {
+  dataSourceOptions = {
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    entities: [User, Book, Review],
+    synchronize: true,
+    logging: false,
+    namingStrategy: new CustomNamingStrategy(),
+  };
+}
+
+const AppDataSource = new DataSource(dataSourceOptions);
 
 export default AppDataSource;
